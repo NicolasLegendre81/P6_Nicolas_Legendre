84218184b4bba0f57e43e04e159598849b0a640b
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-01-17 09:00:58 +0100

    Add files via upload

--- P6_Nicolas_Legendre/backend/middleware/auth.js
+++ P6_Nicolas_Legendre/backend/middleware/auth.js
@@ -7,13 +7,13 @@ module.exports = (req, res, next) => {
     const userId = decodedToken.userId;
     req.userId={ userId}
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+      throw 'userId invalide!';
     } else {
       next();
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: new Error('La requête est invalide!')
     });
   }
 };
